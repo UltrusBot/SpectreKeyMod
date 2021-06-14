ae257e7c28befa73b232bd70decd71a364e6c01a
ultra <13109003+UltrusBot@users.noreply.github.com>
2021-06-14 14:42:56 -0400

    Set up empty mod

---
+++ src/main/java/io/github/ultrusbot/SpectreKeyMod.java
@@ -0,0 +1,11 @@
+package io.github.ultrusbot;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SpectreKeyMod implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		System.out.println("Spectre Key Mod is Initializing!");
+	}
+}
